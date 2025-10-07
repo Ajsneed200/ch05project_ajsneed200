9cde25efdd835f583ac8175831adcd1212c2c6e6
Ajsneed200 <ajsneed200@mail.cfcc.edu>
2025-10-06 23:06:07 -0400

    Update form-validation.js

--- scripts/form-validation.js
+++ scripts/form-validation.js
@@ -1,1 +1,18 @@
+"use strict"
 
+/*check if any reqiured fields are missing */
+
+function checkMissing(formId=main-form) {
+    if (missingFields.length > 0) {
+    alert(`Error - Form not completed`);
+}
+
+} 
+
+/*check if the email is at least 8 characters*/
+
+function validateEmail(formId=main-form) {
+    if (emailInput.value.length < 8) {
+    alert(`Email must contain 8 or more characters`);
+    }
+ }
